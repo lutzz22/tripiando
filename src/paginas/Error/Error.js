54c40c8eef0ac92c0d3228f50da79c6e2fa8d57a
caroflorencia <m.flowercaro@gmail.com>
2022-09-15 12:56:08 -0300

    actualizo pagina error

--- src/paginas/Error/Error.js
+++ src/paginas/Error/Error.js
@@ -1,9 +1,20 @@
 import {React, useContext}  from 'react'
 import '../Error/style.css'
+import error from "../../imagenes/error.jpg"
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='error'>
+      <div className='text-center'>
+        <img src = {error} className='image' alt="..."/>
+      </div>
+      
+      <div className='text-center'>
+        <p className='texto1'> Error 404 </p>
+        <p className='texto2'> página no encontrada </p>
+      </div>
+      
+    </div>
   )
 }
 
