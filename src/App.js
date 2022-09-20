e0cfd447382c5748365f6477d8c683518fc3edee
franaltt <faltomonte@frba.utn.edu.ar>
2022-09-20 14:21:04 -0300

    arreglo index

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import Home from './paginas/Home/Home';
+import Home from './paginas/Home/index';
 import Landing from './paginas/Landing/Landing';
 import Inicio from './paginas/Inicio/Inicio';
 import Explorar from './paginas/Explorar/Explorar';
 import Fav from './paginas/Favoritos/Fav';
 import Perfil from './paginas/Perfil/Perfil';
-import Error from './paginas/Error/index';
+import Error from './paginas/Error/Error';
 import Login from './paginas/Login';
 import Footer from './componentes/Footer/Footer';
 
